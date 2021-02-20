cd59f7a789aace82987a0e226442baf8803de0bf
rekeber <69129780+rekeber@users.noreply.github.com>
2021-02-20 14:15:15 -0600

    Add files via upload

---
+++ usar.js
@@ -0,0 +1,26 @@
+function desayuno(){
+	var desayunas = document.getElementById('desayunas').value;
+
+	if(desayunas==="si"){
+		alert("Que chido");
+	}else{
+		alert("No manches");
+	}
+}
+
+function calificacion(){
+	var calificacion_escrita = document.getElementById('calificacion_escrita').value;
+
+	if(calificacion_escrita==="10"){
+		alert("Excelente :D");
+	}else{
+		alert(":c");
+	}
+}
+
+/*function division(){
+	var primernumero = document.getElementById('primernumero').value;
+	var segundonumero = document.getElementById('segundonumero').value;
+	alert(primernumero/segundonumero);
+	document.getElementById('resultado').innerHTML=primernumero/segundonumero;
+}*/
